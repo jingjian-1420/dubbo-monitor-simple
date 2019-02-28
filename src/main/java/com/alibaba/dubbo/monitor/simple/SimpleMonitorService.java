a1f2018c03a812916acf76758d21f38c54e006d1
wei <632315954@qq.com>
2019-02-28 11:23:05 +0800

    a

--- src/main/java/com/alibaba/dubbo/monitor/simple/SimpleMonitorService.java
+++ src/main/java/com/alibaba/dubbo/monitor/simple/SimpleMonitorService.java
@@ -16,8 +16,9 @@
  */
 package com.alibaba.dubbo.monitor.simple;
 
-import com.alibaba.dubbo.common.Constants;
+
 import com.alibaba.dubbo.monitor.simple.common.CountUtils;
+import org.apache.dubbo.common.Constants;
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.logger.Logger;
 import org.apache.dubbo.common.logger.LoggerFactory;
